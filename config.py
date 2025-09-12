d5b102137b7d2b7116b18bc2214d6e2b996f783e
Nguyen Truong <thainguyentruongpl2@gmail.com>
2025-09-12 23:35:10 +0700

    Add initial configuration file for project settings

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+# Cấu hình chung cho dự án
+
+BASE_URL = "https://congbobanan.toaan.gov.vn/0tat1cvn/ban-an-quyet-dinh"
+BASE_DOMAIN = "https://congbobanan.toaan.gov.vn"
+DATASET_DIR = "./dataset"
+CHECKPOINT_FILE = "checkpoint.json"
+BATCH_SIZE = 1
+NUM_BATCHES = 9
